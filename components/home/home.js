12e09c78d8a11feed5ce8c68a8e5419f2b3b8a25
Zetciak <97193808+Zetciak@users.noreply.github.com>
2023-12-12 02:59:02 +0100

    loader

--- components/home/home.js
+++ components/home/home.js
@@ -1,8 +1,8 @@
 // >> Modules
 import styles from './home.module.scss';
-import Image from 'next/image';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Spline from '@splinetool/react-spline';
+import { Triangle } from 'react-loader-spinner';
 
 import Hero from './hero/hero';
 import AiAnalysis from './aiAnalysis/aiAnalysis';
@@ -14,13 +14,42 @@ import Footer from './footer/footer';
 
 // >> Script
 function Home() {
+	const [robotLoaded, setRobotLoaded] = useState(null);
+	const [openPage, setOpenPage] = useState(false);
+
+	useEffect(() => {
+		if (robotLoaded === 'loaded') {
+			setTimeout(function () {
+				setOpenPage(true);
+			}, 3000);
+		}
+	}, [robotLoaded]);
+
+	function onLoad(spline) {
+		if (spline) {
+			setTimeout(function () {
+				if (spline.dt > 0) {
+					setRobotLoaded('loaded');
+				}
+			}, 100);
+		}
+	}
+
 	return (
 		<div className={styles.page}>
+			<div className={styles.loader} data-visible={robotLoaded}>
+				<div className={styles.triangle}>
+					<Triangle color="#adff00" width="100%" />
+				</div>
+			</div>
 			<div className={styles.heroBg}>
 				<div className={styles.spline}>
-					<Spline scene="https://prod.spline.design/C9DHi814Ir7f4e0H/scene.splinecode" />
+					<Spline
+						scene="https://prod.spline.design/C9DHi814Ir7f4e0H/scene.splinecode"
+						onLoad={onLoad}
+					/>
 				</div>
-				<Hero />
+				{openPage === true ? <Hero /> : null}
 			</div>
 			<AiAnalysis />
 			<Roadmap />
