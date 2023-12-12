8863e2e50340698e5446045ecdbba5cd92405244
Zetciak <97193808+Zetciak@users.noreply.github.com>
2023-12-12 01:06:19 +0100

    robot

--- components/home/home.js
+++ components/home/home.js
@@ -2,6 +2,7 @@
 import styles from './home.module.scss';
 import Image from 'next/image';
 import { useState } from 'react';
+import Spline from '@splinetool/react-spline';
 
 import Hero from './hero/hero';
 import AiAnalysis from './aiAnalysis/aiAnalysis';
@@ -16,6 +17,9 @@ function Home() {
 	return (
 		<div className={styles.page}>
 			<div className={styles.heroBg}>
+				<div className={styles.spline}>
+					<Spline scene="https://prod.spline.design/C9DHi814Ir7f4e0H/scene.splinecode" />
+				</div>
 				<Hero />
 			</div>
 			<AiAnalysis />
